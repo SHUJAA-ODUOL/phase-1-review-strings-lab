9eda6a6064cd69691c91cc20d5c20c1aff8970f0
Shujaa <shujaa.oduol@gmail.com>
2023-09-19 13:51:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Shujaa Oduol'
+const welcomeMessage = `Welcome to Flatbook, ' ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
